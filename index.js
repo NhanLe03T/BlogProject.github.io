9eb900698e51afc603a5c6c36efc71e42334ada0
NhanLe03T <nhanle123.03@gmail.com>
2022-08-26 22:54:19 +0700

    a

--- index.js
+++ index.js
@@ -30,3 +30,52 @@ function NextSlideRight(){
         textImg.innerHTML = "Ẩm thực";
     else textImg.innerHTML = "Lối sống";
 }
+var showInfo = false;
+function show(){
+    let getButton = document.querySelector(".show-info > input");
+    let x = document.querySelector(".rightContent");
+    if(showInfo==false)
+    {
+        x.style.display = `block`;
+        getButton.value = "Ẩn thông tin";
+        showInfo = true;
+    }
+    else{
+        x.style.display = `none`;
+        getButton.value = "Hiện thông tin";
+        showInfo = false;
+    }
+}
+function changePage1(){
+    let page = document.querySelectorAll(".page");
+    let page1 = document.querySelector(".page-1");
+    let pageNumber = document.querySelectorAll(".page-num");
+    for(let i = 0; i<page.length; i++)
+        page[i].style.display="none";
+    page1.style.display = "flex";
+    for(let i = 0; i<pageNumber.length; i++)
+        pageNumber[i].style.backgroundColor = "white";
+    pageNumber[0].style.backgroundColor = "rgb(254, 174, 165)";
+}
+function changePage2(){
+    let page = document.querySelectorAll(".page");
+    let page2 = document.querySelector(".page-2");
+    let pageNumber = document.querySelectorAll(".page-num");
+    for(let i = 0; i<page.length; i++)
+        page[i].style.display="none";
+    page2.style.display = "flex";
+    for(let i = 0; i<pageNumber.length; i++)
+        pageNumber[i].style.backgroundColor = "white";
+    pageNumber[1].style.backgroundColor = "rgb(254, 174, 165)";
+}
+function changePage3(){
+    let page = document.querySelectorAll(".page");
+    let page3 = document.querySelector(".page-3");
+    let pageNumber = document.querySelectorAll(".page-num");
+    for(let i = 0; i<page.length; i++)
+        page[i].style.display="none";
+    page3.style.display = "flex";
+    for(let i = 0; i<pageNumber.length; i++)
+        pageNumber[i].style.backgroundColor = "white";
+    pageNumber[2].style.backgroundColor = "rgb(254, 174, 165)";
+}
